cdcd6fa65f0ea136b1e3e69d375d8f49b08d1416
Maurice Camara <mauricekcamara@gmail.com>
2016-10-06 20:06:04 +0000

    update app

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   get 'welcome/about'
-
-  get 'welcome/contact'
-  
+  get 'welcome/contact'  
   get 'my_friends', to: 'users#my_friends'
+  resources :users, only: [:show]
+  resources :friendships
+  get 'search_friends', to: 'users#search'
+  post 'add_friend', to: 'users#add_friend'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
