7bf779b1d938e0c3fd26bd94ec73cc2b2e03c594
Maurice Camara <mauricekcamara@gmail.com>
2016-10-06 19:35:30 +0000

    updated navigation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+  
+  def full_name
+    return "#{first_name} #{last_name}".strip if (first_name || last_name)
+    "Anonymous"
+  end
 
 
 
