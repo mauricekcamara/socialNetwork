1661df06c8a666e6c32cde1138e08e0bf567ffd5
Maurice Camara <mauricekcamara@gmail.com>
2016-10-07 02:16:12 +0000

    fixed email confirmation view

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -2,6 +2,10 @@ class User::RegistrationsController < Devise::RegistrationsController
   before_filter :configure_permitted_parameters
 
   protected
+  
+  def after_inactive_sign_up_path_for(resource)
+    new_user_session_path
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
