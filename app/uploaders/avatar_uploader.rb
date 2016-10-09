92958d6b1f34291e22d462a6905b23b957007697
Maurice Camara <mauricekcamara@gmail.com>
2016-10-09 16:45:55 +0000

    add production image upload

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,5 +1,5 @@
 class AvatarUploader < CarrierWave::Uploader::Base
-  
+
   include CarrierWave::MiniMagick
   process resize_to_limit: [300, 300]
 
@@ -8,8 +8,11 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
