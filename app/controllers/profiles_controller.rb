817238db9e0e045cbfea0a700f924b2577312c6f
Adam <ashortsleeves@gmail.com>
2016-10-21 15:56:28 +0000

    began cancan abilities

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,7 @@
 class ProfilesController < ApplicationController
   before_action :set_profile, only: [:show, :edit, :update, :destroy]
-
+  before_filter :authenticate_user!
+  load_and_authorize_resource
   # GET /profiles
   # GET /profiles.json
   def index
