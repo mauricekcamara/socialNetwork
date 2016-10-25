1e52b9725b18ea3a7cca8bf9808e228d8fdbf85c
Maurice Camara <mauricekcamara@gmail.com>
2016-10-25 17:07:04 +0000

    fixing the conversations index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  helper_method :get_the_user
+  DummyUser = Struct.new(:full_name)
+
+  def get_the_user(id)
+    user = User.find_by(id: id)
+    return user if user
+    user = DummyUser.new('deleted user')
+  end
 end
