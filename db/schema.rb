49bda3677d496791aacb0f1d496dc84656caafe0
ashortsleeves <ashortsleeves@gmail.com>
2016-10-25 14:43:22 -0400

    education section added, year does not display

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161025164521) do
+ActiveRecord::Schema.define(version: 20161025180009) do
 
   create_table "conversations", force: :cascade do |t|
     t.integer  "sender_id"
