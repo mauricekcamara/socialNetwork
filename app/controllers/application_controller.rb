86783d4627cca9d4b5143a2715d58ce8cd3b0186
Maurice Camara <mauricekcamara@gmail.com>
2016-10-27 23:52:06 +0000

    fixed some weird error in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-<<<<<<< HEAD
   rescue_from CanCan::AccessDenied do |exception|
   redirect_to main_app.root_url, :alert => exception.message
 end
-=======
 
   helper_method :get_the_user
   DummyUser = Struct.new(:full_name)
